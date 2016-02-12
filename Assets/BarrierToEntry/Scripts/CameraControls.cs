42295b055d7c1e7aa6784715597f076eee1b2a13
Albert Johnston <ajohn104@lion.lmu.edu>
2016-02-11 17:03:18 -0800

    Added initial saber sounds

--- Assets/BarrierToEntry/Scripts/CameraControls.cs
+++ Assets/BarrierToEntry/Scripts/CameraControls.cs
@@ -1,35 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 using Coliseo;
+using BarrierToEntry;
 
-public class CameraControls : MonoBehaviour {
+namespace BarrierToEntry
+{
+    public class CameraControls : MonoBehaviour
+    {
 
-    public Transform camTrans;
+        public Transform camTrans;
 
-    // Mouse sensitivity
-    public float horizontalSpeed = 4.0F;
-    public float verticalSpeed = 4.0F;
+        // Mouse sensitivity
+        public float horizontalSpeed = 4.0F;
+        public float verticalSpeed = 4.0F;
 
-    // Use this for initialization
-    void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        float h = horizontalSpeed * Input.GetAxis("Mouse X");
-        float v = verticalSpeed * Input.GetAxis("Mouse Y");
-
-        float rotationX = h*Time.deltaTime;
-        float rotationY = v*Time.deltaTime;
-
-        if (Application.platform == RuntimePlatform.WindowsPlayer)
+        // Use this for initialization
+        void Start()
         {
-            camTrans.Rotate(-rotationY, rotationX, 0);
+
         }
-        if (Application.platform == RuntimePlatform.WindowsEditor)
+
+        // Update is called once per frame
+        void Update()
         {
-            camTrans.Rotate(-rotationY/verticalSpeed*40f, rotationX / horizontalSpeed * 40f, 0);
+            float h = horizontalSpeed * Input.GetAxis("Mouse X");
+            float v = verticalSpeed * Input.GetAxis("Mouse Y");
+
+            float rotationX = h * Time.deltaTime;
+            float rotationY = v * Time.deltaTime;
+
+            if (Application.platform == RuntimePlatform.WindowsPlayer)
+            {
+                camTrans.Rotate(-rotationY, rotationX, 0);
+            }
+            if (Application.platform == RuntimePlatform.WindowsEditor)
+            {
+                camTrans.Rotate(-rotationY / verticalSpeed * 40f, rotationX / horizontalSpeed * 40f, 0);
+            }
         }
     }
 }
